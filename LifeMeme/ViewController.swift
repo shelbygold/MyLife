d59dd9e8440e1a0803246ceca6501a77e345900c
shelby gold <shelbygold60@gmail.com>
2019-08-27 16:34:41 -0600

    Initial Commit

---
+++ LifeMeme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeMeme
+//
+//  Created by shelby gold on 8/27/19.
+//  Copyright © 2019 AppsVentured. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
